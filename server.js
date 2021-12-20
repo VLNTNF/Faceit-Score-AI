c92a0dd6c25d1bf82347b8e911a6379f6a24c5da
Valentin F <valentin.fosse.76@gmail.com>
2021-12-20 18:24:01 +0100

    Server tests

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-'use strict';
-const bodyparser = require('body-parser');
+'use strict'
 const express = require('express');
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 
+server.use(express.json());
+
 server.listen(PORT, () => console.log(`Service running on Port ${PORT}`));
-server.post('/', (request, response, data) => {
-    console.log(data);
-    console.log('\n\n\n\n\n\n')
+server.post('/', (req, res) => {
+  var date = new Date().toLocaleString();
+  console.log('\n\n\n---------------------\n\n\n' + date + '\n\n');
+  console.log(req);
+  res.sendStatus(200);
 });
